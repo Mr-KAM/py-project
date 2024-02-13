b984b75c0cb5bc5963d2303ef1d43b081e8a3e41
Anicet Cyrille KAMBOU <67423679+Mr-KAM@users.noreply.github.com>
2024-02-13 02:43:13 +0000

    Modification de py-project.bat et de Readme.md

--- script.py
+++ script.py
@@ -5,5 +5,6 @@ import shutil
 os.chdir(os.getcwd())
 sources = ["py-project.py", "py-project.bat"]
 destination = os.path.dirname(sys.executable)  # Executable de python
+print(destination)
 for source in sources:
     shutil.copy(source, destination)
